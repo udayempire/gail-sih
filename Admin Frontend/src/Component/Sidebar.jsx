0ee1ef2cf449e4fb415cee8982dc73a21a138939
udayempire <kuday0726@gmail.com>
2024-09-07 05:52:40 +0530

    added Manual checking page

--- Admin Frontend/src/Component/Sidebar.jsx
+++ Admin Frontend/src/Component/Sidebar.jsx
@@ -1,16 +1,19 @@
-import { Link } from "react-router-dom"
-export function Sidebar() {
-    return <div className="w-60 bg-home-left p-4 h-full">
-            <div className="flex flex-col gap-10 my-10 font-semibold text-2ndfont ">
-                    <Link to="/insights" className="hover:text-black">Insights</Link>
-                    <Link to="/addemployee" className="hover:text-black">Add New Employee</Link>
-                    <Link to="/updateemployee" className="hover:text-black">Update Employee details</Link>
-                    <Link to="/manualcheckin" className="hover:text-black">Manual Check-In Approval</Link>
-                    <Link to="/leaveapproval" className="hover:text-black">Leave Approval</Link>
-                    <Link to="/employeereport" className="hover:text-black">Employee Report</Link>
-                    <Link to="/activity" className="hover:text-black">Recent Activity</Link>
-                    <Link to="/login" className="hover:text-black">LogIn / LogOut</Link>
-            </div>
-    </div>
+import { Link } from "react-router-dom";
 
+export function Sidebar() {
+    return (
+        <div className="w-60 bg-home-left p-4 h-full">
+            <div className="flex flex-col gap-10 my-10 font-semibold text-2ndfont">
+                {/* Apply hover:text-white to all links */}
+                <Link to="/insights" className="hover:text-white">Insights</Link>
+                <Link to="/addemployee" className="hover:text-white">Add New Employee</Link>
+                <Link to="/updateemployee" className="hover:text-white">Update Employee details</Link>
+                <Link to="/manualcheckin" className="hover:text-white">Manual Check-In Approval</Link>
+                <Link to="/leaveapproval" className="hover:text-white">Leave Approval</Link>
+                <Link to="/employeereport" className="hover:text-white">Employee Report</Link>
+                <Link to="/activity" className="hover:text-white">Recent Activity</Link>
+                <Link to="/login" className="hover:text-white">LogIn / LogOut</Link>
+            </div>
+        </div>
+    );
 }
--- Admin Frontend/src/pages/ManualCheckin.jsx
+++ Admin Frontend/src/pages/ManualCheckin.jsx
@@ -1,5 +1,5 @@
+import { Checkinbar } from "../Component/Checkinbar";
 import { Navbar } from "../Component/Navbar";
-import { Recentbar } from "../Component/Recentbar";
 import {  Sidebar } from "../Component/Sidebar";
 
 
@@ -14,9 +14,9 @@ export const ManualCheckin = () => {
                 <div className="flex-1 p-8 ">
                     {/* Center the content in the middle of the main content area */}
                     <div className="flex justify-between  ">
-                        <h1 className="font-bold text-2xl mb-4 text-1stfont">Recent Activity</h1>
+                        <h1 className="font-bold text-2xl mb-4 text-1stfont">Manual Check-In Approval</h1>
                         <div className="flex gap-2">
-                            <h2 className="font-semibold text-1stfont ">Work Campus :</h2>
+                            <h2 className="font-bold text-1stfont ">Work Campus :</h2>
                             <select className="bg-white relative px-4 w-60 rounded-xl flex items-center font-semibold bottom-3">
                                 <option>Vijyapur , MP</option>
                                 <option>Bhopal , Madhya Pradesh</option>
@@ -25,18 +25,19 @@ export const ManualCheckin = () => {
                     </div>
                     <div className="bg-white rounded-md " >
                         <div className="relative right-14 top-4">
-                            <div className="flex justify-around font-bold text-xl text-1stfont">
+                            <div className="flex justify-around font-semibold text-xl text-1stfont">
                                 <div className="relative left-16">Employee</div>
                                 <div className="flex gap-20">
-                                    <div className="relative left-6">In-Time</div>
+                                    <div className="relative left-16">In-Time</div>
                                     <div className="relative left-6">Out-Time</div>
                                     <div className="relative left-7">Working Hour</div>
                                     <div className="relative right-3">Last Location</div>
+                                    <div className="relative right-3">Activity</div>
                                 </div>
                             </div>
                         </div>
                         <div>
-                            <Recentbar/>
+                            <Checkinbar/>
 
                         </div>
                     </div>
