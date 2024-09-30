5c3e61ebd848143f00c756ece17e4deb93d4fea9
udayempire <kuday0726@gmail.com>
2024-09-30 16:08:18 +0530

    fixed navbar

--- Admin Frontend/src/Component/Navbar.jsx
+++ Admin Frontend/src/Component/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Button } from "./Button"
 export function Navbar(){
     return <div>
-        <div className="p-2 px-8 w-screen bg-white flex justify-between z-50 h-18">
+        <div className="p-[-1.25px] px-8 w-screen bg-white flex justify-between z-50 h-18">
             <img src="gtrack-nav.svg" alt="" className="relative left-36" />
             <Button label={"Download App"}/>
 
