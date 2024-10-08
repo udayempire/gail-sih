e875a3b3614e891e710041bd3b273c4bb3334a83
udayempire <kuday0726@gmail.com>
2024-10-08 09:06:00 +0530

    app can be downloaded

--- Admin Frontend/src/Component/Navbar.jsx
+++ Admin Frontend/src/Component/Navbar.jsx
@@ -1,10 +1,14 @@
-import { Button } from "./Button"
-export function Navbar(){
-    return <div>
-        <div className="p-[-1.25px] px-8 w-screen bg-white flex justify-between z-50 h-18">
-            <img src="gtrack-nav.svg" alt="" className="relative left-36" />
-            <Button label={"Download App"}/>
+import { Button } from "./Button";
 
+export function Navbar() {
+    return (
+        <div>
+            <div className="p-[-1.25px] px-8 w-screen bg-white flex justify-between z-50 h-18">
+                <img src="gtrack-nav.svg" alt="" className="relative left-36" />
+                <a href="/gtrrack.apk" download>
+                    <Button label={"Download App"} />
+                </a>
+            </div>
         </div>
-    </div>
+    );
 }
